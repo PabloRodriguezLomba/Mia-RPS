34156e06979a9203f0c47b4ef6c136b8d1ee9152
Pablorl <Pablorodlom@gmail.com>
2025-01-07 21:18:07 +0100

    feat(test_rpsls.py): arregle algunos test los cuales estaban mal planteados

--- test/test_rpsls.py
+++ test/test_rpsls.py
@@ -10,7 +10,7 @@ def test_rpsls02():
     last_play = {"Victory":2,"rival_action":4,"computer_action":3}
     past_actions.append(last_play)
     computer_action = get_computer_action()
-    assert computer_action == 2    
+    assert computer_action == 0    
 
 def test_rpsls03():
     last_play = {"Victory":0,"rival_action":1,"computer_action":5}
@@ -22,7 +22,7 @@ def test_rpsls04():
     last_play = {"Victory":2 ,"rival_action":0,"computer_action":4}
     past_actions.append(last_play)
     computer_action = get_computer_action()
-    assert computer_action == 3  
+    assert computer_action == 0  
 
 def test_rpsls05():
     last_play = {"Victory":0,"rival_action":3,"computer_action":1}
